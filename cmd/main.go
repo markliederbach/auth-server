b52f18ed2e6d805a68aa7e1f0f04f99ab51bc610
markliederbach <10621536+markliederbach@users.noreply.github.com>
2020-11-02 22:49:15 +0000

    gin

--- cmd/main.go
+++ cmd/main.go
@@ -6,17 +6,19 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type server struct{}
-
 func main() {
+	// Core router
 	router := gin.Default()
 
+	// Versioned API group
 	v1 := router.Group("/v1")
 	v1.GET("/ping", pingV1)
 
-	router.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// Serve on default port (8080)
+	router.Run()
 }
 
+// route handler
 func pingV1(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
